5e43e0b1f6743b370926b1ed57e92c355ab8a477
George Danezis <george@danez.is>
2022-05-26 17:10:35 +0100

    Added poor man's checkpoint sync

--- crates/sui-core/src/authority/authority_store.rs
+++ crates/sui-core/src/authority/authority_store.rs
@@ -363,11 +363,10 @@ impl<
             .ok_or(SuiError::TransactionLockDoesNotExist)?;
 
         match transaction_option {
-            Some(tx_digest) => Ok(Some(
-                self.transactions
-                    .get(&tx_digest)?
-                    .expect("Stored a lock without storing transaction?"),
-            )),
+            Some(tx_digest) => {
+                return Ok(self.transactions.get(&tx_digest)?);
+                // .expect("Stored a lock without storing transaction?"),
+            }
             None => Ok(None),
         }
     }
